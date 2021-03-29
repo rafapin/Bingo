8f9c100023916824653b453a480e7082b22835b9
Rafael Pinto <rafapin97@hotmail.com>
2021-03-28 20:30:54 -0500

    First Commit

---
+++ Bingo/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bingo.Models;
+
+namespace Bingo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public List<Balota> Balotas = new List<Balota>();
+
+        public class Balota
+        {
+            public int Number { get; set; }
+            public int Status { get; set; }
+
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int Reset()
+        {
+            for(var i=1; i <= 75; i++)
+            {
+                Balotas.Add(new Balota { Number = 1, Status = 0 });
+            }
+            return 1;
+        }
+        public List<Balota> GetBalotas()
+        {
+            return Balotas;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bingo/Hubs/BingoHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bingo.Hubs
+{
+    public class BingoHub:Hub
+    {
+        public static int contador = 0;
+        public static List<string> users = new List<string>();
+
+        public override Task OnConnectedAsync()
+        {
+            contador++;
+            Clients.All.SendAsync("MostrarConectados", contador);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            contador--;
+            Clients.All.SendAsync("MostraConectados", contador);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendNumber(int number)
+        {
+            await Clients.Others.SendAsync("ReceiveNumber", number);
+        }
+
+        public async Task AddList(string name)
+        {
+            users.Add(name);
+            await Clients.All.SendAsync("ListUsers", users);
+        }
+    }
+}
